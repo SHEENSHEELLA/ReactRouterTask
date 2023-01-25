891f3e2286535a14b22a9fc842a83b408977798d
Dzmitry Sashcheka <sheensheella@gmail.com>
2023-01-25 19:03:47 +0300

    Use parameters in routes and the useParams Hook

--- src/components/SingleCourse.jsx
+++ src/components/SingleCourse.jsx
@@ -1,5 +1,19 @@
+import { Link, useParams } from 'react-router-dom';
+import courses from '../date/courses';
+
 const SingleCourse = () => {
-  return <h1>Single Course Info</h1>;
+  const params = useParams();
+  const course = courses.find((course) => (course.slug = params.slug));
+  return (
+    <>
+      <h1>{course.title}</h1>
+      <h2>{course.slug}</h2>
+      <h3>{course.id}</h3>
+      <Link to=".." relative="path">
+        All courses
+      </Link>
+    </>
+  );
 };
 
 export default SingleCourse;
