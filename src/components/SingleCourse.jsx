c67039c01d76dd1bcbec0ef97ab22ae9ecb611a9
Dzmitry Sashcheka <sheensheella@gmail.com>
2023-01-28 00:05:40 +0300

    Redirect with hook useNavigate

--- src/components/SingleCourse.jsx
+++ src/components/SingleCourse.jsx
@@ -1,18 +1,23 @@
-import { Link, useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import courses from '../date/courses';
 import NotFound from './NotFound';
 
 const SingleCourse = () => {
   const params = useParams();
+  const navigate = useNavigate();
   const course = courses.find((course) => course.slug === params.slug);
-  if (!course) {
-    return <NotFound />;
-  }
+  useEffect(() => {
+    if (!course) {
+      navigate('..', { relative: 'path' });
+    }
+  }, [course]);
+
   return (
     <>
-      <h1>{course.title}</h1>
-      <h2>{course.slug}</h2>
-      <h3>{course.id}</h3>
+      <h1>{course?.title}</h1>
+      <h2>{course?.slug}</h2>
+      <h3>{course?.id}</h3>
       <Link to=".." relative="path">
         All courses
       </Link>
